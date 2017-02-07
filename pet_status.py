4df69e8486a96c56af8972908ca75ced833134c3
Ellen <chenellen007@gmail.com>
2017-02-06 22:04:15 -0500

    Add files via upload

---
+++ pet_status.py
@@ -0,0 +1,16 @@
+class PetStatus:
+    """ A class containing the status of a Pet
+
+    """
+
+    def __init__(self, pet):
+        pass
+
+    def stat_change_n(self, a):
+        pass
+
+    def stat_change_d(self, a):
+        pass
+
+    def stat_change_f(self, a):
+        pass
